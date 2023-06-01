938897ae7e13e0a805224dad839ecac610bfafa5
Minjik06 <minjik525@deltalearns.ca>
2023-06-01 10:52:33 -0700

    152

--- task1.py
+++ task1.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from typing import Text
 import numpy as np
+import csv
 """
 Task 1
 Read the map1.txt file and convert to a map that you can navigate a
@@ -12,7 +13,17 @@ w.attributes('-topmost',True)
 c = tk.Canvas(height=475,width=900,bg="#ffdddd")
 c.pack()
 
-f = open('map1.txt')
+
+csv_filename = 'map1.txt'
+with open(csv_filename) as f:
+    reader = csv.reader(f)
+    lst = list(reader)
+    print(lst)
+    for i in range(len(lst)):
+        print(i,lst[i])
+
+
+"""f = open('map1.txt')
 f=f.readlines()
 print(f)
 
@@ -20,8 +31,8 @@ k=[15][29]
 
 for i in range(15):
     for j in range(29):
+        f[i]=str(f[i])
         f[i].__len__(j) = k[i][j]
 print(k)
 
-
-w.mainloop()
+w.mainloop()"""
