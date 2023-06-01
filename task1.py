61d301c04a2b075255dde70e06b1e2cf21de980a
Minjik06 <minjik525@deltalearns.ca>
2023-06-01 10:44:16 -0700

    dfdf

--- task1.py
+++ task1.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from typing import Text
+import numpy as np
 """
 Task 1
 Read the map1.txt file and convert to a map that you can navigate a
@@ -10,9 +11,17 @@ w.geometry("925x475")
 w.attributes('-topmost',True)
 c = tk.Canvas(height=475,width=900,bg="#ffdddd")
 c.pack()
+
 f = open('map1.txt')
+f=f.readlines()
+print(f)
 
+k=[15][29]
 
+for i in range(15):
+    for j in range(29):
+        f[i].__len__(j) = k[i][j]
+print(k)
 
 
 w.mainloop()
