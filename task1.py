4abe510f9d97b386503e2a9136cdd610e3f8e220
Minjik06 <minjik525@deltalearns.ca>
2023-06-02 10:29:43 -0700

    dfsdfs

--- task1.py
+++ task1.py
@@ -2,24 +2,64 @@ import tkinter as tk
 from typing import Text
 import numpy as np
 import csv
+from PIL import ImageTk,Image
 """
 Task 1
 Read the map1.txt file and convert to a map that you can navigate a
 rectangle object through.
 """
 w = tk.Tk()
-w.geometry("1000x1000")
+w.geometry("950x530")
 w.attributes('-topmost',True)
-c = tk.Canvas(height=1500,width=1500,bg="#ffdddd")
+c = tk.Canvas(height=530,width=950,bg="#ffdddd")
 c.pack()
-f=open('map1')
-map1=f.readlines
+f=open('map1.txt')
+map1=f.readlines()
 
-for i in range(1,15):
-    for fd
+
+"""def getImage(x,y):
+    img = Image.open("assets/map.swamp.png").convert("RGBA")
+    xi = x*64
+    yi = y*64
+    img2 = img.crop([xi,yi,xi+64,yi+64])
+    return ImageTk.PhotoImage(img2)"""
+
+
+walls=[]
+img = [[]]
+k=0
+p=0
+for i in range(1,2):
+    for i in range(len(map1)):
+        print(map1[i])
+        p=0
+        k+=30
+        for j in range(len(map1[i])):
+            p+=30
+            if map1[i][j]=="*":
+                print(f"map1: {j},{i}")
+                c.create_rectangle(j+p,i+k,j+p+30,i+k+30, fill='green')
+                
+                
+#c.create_rectangle(i+p,10+j+k,i+p+5,10+j+k+5, fill='green')
 #c.create_rectangle(300,300,320,340, fill='white')
-
-
+"""
+for i in range(1,2):
+    for i in range(len(map1)):
+        print(map1[i])
+        k=0
+        p+=10
+        for j in range(len(map1[i])):
+            if map1[i][j]=="*":
+                print(f"map1: {j},{i}")
+                
+                k+=20
+"""
+"""i=i+k
+            j=j+k
+            c.create_rectangle(i,j,i+5,j+5, fill='white')
+            k*7"""
+#c.create_rectangle(i*i,j*j,i*i+5,j*j+5, fill='white')
 """sv_filename = 'map1.txt'
 with open(csv_filename) as f:
     reader = csv.reader(f)
